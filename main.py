d21d7f46b751ffd15c483d4cd9412894a68a847a
rc1200 <ron.calibuso@gmail.com>
2019-09-25 02:46:07 -0400

    created cheat sheet on how to use bs4... Need to progress that with real scraping program

--- main.py
+++ main.py
@@ -2,33 +2,28 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+
+# store entire object into a variable
 page = requests.get(
     'https://forecast.weather.gov/MapClick.php?lat=41.8843&lon=-87.6324#.XIRQYFNKgUE'
 )
 
-print(page)
-soup = BeautifulSoup(page.content, 'html.parser') # lets the object know we are passing an HTML and to pars it as that
-print(soup)
+# lets the object know we are passing an HTML and to pars it as that
+soup = BeautifulSoup(page.content, 'html.parser')
 
-'''
+# grabbing all the code inside the div based on the ID
 week = soup.find(id='seven-day-forecast-body')
-# print(week)
 
+# store everything within the container
+# NOTE since we are using Class container instead of a tag we use "class_" since class is reserved
 items = week.find_all(class_='tombstone-container')
-# print(items[0])
 
-# print(items[0].find(class_='period-name').get_text())
-# print(items[0].find(class_='short-desc').get_text())
-# print(items[0].find(class_='temp').get_text())
-
+# storing the text into a list using list comprehension
 period_names = [item.find(class_='period-name').get_text() for item in items]
 short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
 temperatures = [item.find(class_='temp').get_text() for item in items]
 
-# print(period_names)
-# print(short_descriptions)
-# print(temperatures)
-
+# passing a dictionary using Pandas Dataframe ... has the Key and List
 weather_stuff = pd.DataFrame(
     {
         'period': period_names,
@@ -38,7 +33,9 @@ weather_stuff = pd.DataFrame(
 
 print(weather_stuff)
 
-weather_stuff.to_csv('qazi_gift_ebay.csv')
+# export the data into a csv file
+weather_stuff.to_csv('exported_to_csv.csv')
 
+'''
 
 '''
