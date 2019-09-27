0fe8f80ce70709cc5402af788523f30c29b51493
rc1200 <ron.calibuso@gmail.com>
2019-09-26 23:37:22 -0400

    was able to get data now, seems Amazon had issues before.  Store it to Dict, then aggregate all of them to dataframe

--- main.py
+++ main.py
@@ -44,83 +44,97 @@ weather_stuff.to_csv('exported_to_csv.csv')
 
 # store entire object into a variable
 
-
+url = 'https://www.amazon.ca/gp/offer-listing/{}'.format('007738248X')
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-page = requests.get('https://www.amazon.ca/gp/offer-listing/007738248X', headers=headers)
-# page = requests.get('https://www.amazon.ca/gp/offer-listing/007738248X')  NOTE: you might get this if you don't add headers 503 Server Error: Service Unavailable for url: https://www.amazon.ca/gp/offer-listing/007738248X
-
+response = requests.get(url, headers=headers)
+# response = requests.get('https://www.amazon.ca/gp/offer-listing/007738248X')  NOTE: you might get this if you don't add headers 503 Server Error: Service Unavailable for url: https://www.amazon.ca/gp/offer-listing/007738248X
 
 try:
-    page.raise_for_status()
+    response.raise_for_status()
 except requests.exceptions.HTTPError as e:
     print(e)
 
 
 # lets the object know we are passing an HTML and to pars it as that
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# print(page)
-# print(soup)
+soup = BeautifulSoup(response.content, 'lxml')  # note for some reason html.parser was not getting all the data
 
 
-Alloffers = soup.find(id='olpRefinements')
-print(Alloffers)
-
-# store everything within the Offers
-# NOTE since we are using Class container instead of a tag we use "class_" since class is reserved
-
-
-# items = Alloffers.find_all(class_='a-size-large a-color-price olpOfferPrice a-text-bold')
-# print(items)
-# print(items[0])
+# backup method if we needed to put a delay and actually open the page
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# browser = webdriver.Chrome()  # ensure the chrome driver is install in the same directory or explictly specify path
+# browser.get(url)
+# soup = browser.page_source
 
 
-# storing the text into a list using list comprehension
-# uisng .text.strip() vs .get_text() to strip out the spaces
+# all the offers for each ROW is stored in this Div class
+# creates a list of bojects which we will further parse out
+Alloffers = soup.find_all(attrs={'class': 'olpOffer'})
 
 
-# price = [item.find(class_='a-size-large a-color-price olpOfferPrice a-text-bold').text.strip() for item in items] seems only for big price, not accurate as it doesn't include shipping
-
-# price = [item.find(class_='a-row a-spacing-mini olpOffer').get_text() for item in items]
-
-# print(price)
-
-
-'''
-# Class for entire row... but need to filter based on specific columns
-'a-row a-spacing-mini olpOffer'
-
-# Class Filter for Price n.b. there could be 2 prices for shipping, maybe use regex to get total amokunt /d
-'a-column a-span2 olpPriceColumn'
-
-# Class Filter for Condition
-'a-column a-span3 olpConditionColumn'
-
-# Class Filter for Seller Information
-'a-column a-span2 olpSellerColumn'
-
-# Class Filter for Delivery
-'a-column a-span3 olpDeliveryColumn'
-'''
-
-# ------------------------------
+# safeguad when fetching data if type is NONE ie. there is no text (ie. Shipping olpShippingPrice class)
+def getText(mainDiv, className):
+    if mainDiv.find(attrs={'class': className}) is not None:
+        return mainDiv.find(attrs={'class': className}).text.strip()
+    else:
+        return '0'
 
 
-def getAmazonPrice(productUrl):
+def getData(offer_list_index):
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', }
+    sellerData = {
+        # price = getText(offer_list_index, 'olpOfferPrice')
+        # priceShipping = getText(offer_list_index, 'olpShippingPrice')
+        # condition = getText(offer_list_index, 'olpConditionColumn')
+        # seller = getText(offer_list_index, 'olpSellerColumn')
+        # delivery = getText(offer_list_index, 'olpDeliveryColumn')
+        'price': getText(offer_list_index, 'olpOfferPrice'),
+        'priceShipping': getText(offer_list_index, 'olpShippingPrice'),
+        'condition': getText(offer_list_index, 'olpConditionColumn'),
+        'seller': getText(offer_list_index, 'olpSellerColumn'),
+        'delivery': getText(offer_list_index, 'olpDeliveryColumn')
+    }
 
-    res = requests.get(productUrl, headers=headers)
-    # res = requests.get(productUrl)
-    try:
-        res.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(e)
+    for k, v in sellerData.items():
+        print('{} \t\t:----> {}'.format(k, v))
 
-    soup = BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#newOfferAccordionRow .header-price')
-    return elems[0].text.strip()
+    return sellerData
 
 
-# price = getAmazonPrice('http://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=&sr=')
-# print('The price is ' + price)
+print('\n\n\n')
+print('all offers {}'.format(len(Alloffers)))
+
+myPanda = pd.DataFrame()
+for i in Alloffers:   # for i in range(len(Alloffers)):
+    myPanda = myPanda.append(getData(i), ignore_index=True)
+
+
+# myPanda = myPanda.append(getData(Alloffers[2]), ignore_index=True)
+# myPanda = pd.DataFrame(getData(Alloffers[1]), index=[0])
+
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  my Panda ???????????????')
+print(myPanda)
+print(myPanda[['price', 'priceShipping']])
+
+
+# print(getData(Alloffers[1]))
+# getData(Alloffers[5])
+# getData(Alloffers[2])
+
+# for offer in Alloffers:
+#     print(offer)
+
+
+def justTesting():
+
+    items = Alloffers[-1].find_all(class_='tombstone-container')
+
+    # storing the text into a list using list comprehension
+    period_names = [item.find(class_='period-name').get_text() for item in items]
+    short_descriptions = [item.find(class_='short-desc').get_text() for item in items]
+    temperatures = [item.find(class_='temp').get_text() for item in items]
+
+    print(period_names)
+    print(short_descriptions)
+    print(temperatures)
