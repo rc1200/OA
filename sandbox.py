ebb6ac689644aac5e7453da404c15cea1540e9a4
rc1200 <ron.calibuso@gmail.com>
2019-10-17 13:31:06 -0400

    worked on testing using Pandas, success. Now create framework

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,6 @@
 import pandas as pd
 ronDict = {'0133356728': {'price_canada': 58.7, 'Condition_canada': 'Used - Very Good', 'price_usa': -99, 'Condition_usa': 'something wrong happened'},
-'2222': {'price_canada': 58.7, 'Condition_canada': 'Used - Very Good', 'price_usa': -99, 'Condition_usa': 'something wrong happened'}}
+           '2222': {'price_canada': 50, 'Condition_canada': 'Used - Very Good', 'price_usa': 75, 'Condition_usa': 'something wrong happened'}}
 
 
 # ronDict = {'price1': 58.7, 'Condition1': 'Used - Very Good', 'price2': -99, 'Condition2': 'something wrong happened'}
@@ -9,10 +9,19 @@ ronDict = {'0133356728': {'price_canada': 58.7, 'Condition_canada': 'Used - Very
 print(ronDict)
 
 
-test = pd.DataFrame.from_dict(ronDict, orient='index')
-print(test)
+testDf = pd.DataFrame.from_dict(ronDict, orient='index')
+print(testDf)
 
 
-# data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
-# test = pd.DataFrame.from_dict(data)
-# print(test)
+def pct_gain(x, y): return (y - x) / x
+
+
+testDf["ProfitFactor"] = pct_gain(testDf.price_canada, testDf.price_usa)
+print(testDf)
+
+# diff = testDf.price_canada.apply(pct_gain2)
+# print(diff)
+
+
+# testDf["NewCol"] = testDf.price_canada.apply(pct_gain, y=testDf.price_usa)
+# print(testDf)
