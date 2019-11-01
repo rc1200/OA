5d4ca3b9e733ebaf16be7e5a6bcb6ee4ad606758
rc1200 <ron.calibuso@gmail.com>
2019-11-01 01:37:45 -0400

    simple home page

--- oaApp/cadusApp/urls.py
+++ oaApp/cadusApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('super', views.super, name='super'),
+    # path('super', views.super, name='super'),
 ]
--- oaApp/cadusApp/views.py
+++ oaApp/cadusApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from . cadus_utilities.test import runSuperCode
-from time import sleep
+
 
 
 def home(request):
@@ -10,11 +10,11 @@ def home(request):
     return render(request, 'home.html', {'sendtohtml' : sendtohtml})
 
 
-def super(request):
-    # sendtohtml = [1,2,3,4]
-    sendtohtml = ['all','done','now',4]
-    # runSuperCode()
-    print('sendtohtml')
-    print(sendtohtml)
-    return render(request, 'home.html', {'sendtohtml' : sendtohtml})
-    # return redirect(home)
+# def super(request):
+#     # sendtohtml = [1,2,3,4]
+#     sendtohtml = ['all','done','now',4]
+#     # runSuperCode()
+#     print('sendtohtml')
+#     print(sendtohtml)
+#     return render(request, 'home.html', {'sendtohtml' : sendtohtml})
+#     # return redirect(home)
